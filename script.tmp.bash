0ffe215a967d3e1f0edd839a9a24cd2465d68e3c
whybe <vvhybe@hotmail.com>
2025-02-23 20:38:24 +0000

    Update script.tmp.bash

--- script.tmp.bash
+++ script.tmp.bash
@@ -8,6 +8,11 @@
 
 readonly VERSION="v1.0.0"
 
+# Dependencies
+readonly DEPS=(
+    # Add dependencies here, or leave empty if none
+)
+
 # Script Manual
 usage() {
     cat <<EOF
@@ -18,14 +23,31 @@ Description of the script.
 Options:
   -h, --help      Show this help message and exit.
   -v, --version   Show the script version and exit.
-  <other options> Description of other options.
+  --deps          List all dependencies for this script (pipe-friendly).
 
 Examples:
   ${0##*/} --option1
   ${0##*/} --option2
 
+To install dependencies:
+  ${0##*/} --deps | sudo pacman -S -
+  ${0##*/} --deps | sudo apt install -
+  ${0##*/} --deps | pip install -r /dev/stdin
+
 Report bugs to: <your email or GitHub issues>
 EOF
+}
+
+# Function to list dependencies (pipe-friendly)
+list_deps() {
+    if [[ ${#DEPS[@]} -eq 0 ]]; then
+        echo "This script has no dependencies."
+        return 0
+    fi
+
+    for dep in "${DEPS[@]}"; do
+        echo "$dep"
+    done
 }
 
 # Parse command-line arguments
@@ -37,6 +59,10 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
             ;;
         -v | --version)
             echo "${0##*/} $VERSION"
+            exit 0
+            ;;
+        --deps)
+            list_deps
             exit 0
             ;;
         *)
